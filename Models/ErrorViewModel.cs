12fbaaa74f2c09a190293670638b60d0c48f4cee
Kaan Kaya <151462786+Hr0dgeir@users.noreply.github.com>
2025-02-21 20:08:57 +0300

    2. Temeller Bölümü Ders 2.5 MVC dosyaları oluşturma

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Performans_WTUG.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
