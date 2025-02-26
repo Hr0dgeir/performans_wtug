238a52b22e2e3ede223c51f376b2c8babeb99f41
Kaan Kaya <151462786+Hr0dgeir@users.noreply.github.com>
2025-02-26 22:05:08 +0300

    4. Veri Odakli Geliştirme bölümü ders 4.3 MVC yapısına uygun Dizinler ve dosyalar oluşturuldu yönlendirme kodları yazıldı

--- Veri_Odakli_Gelistirme/Store/StoreApp/obj/Debug/net8.0/Veri_Odakli_Gelistirme.AssemblyInfo.cs
+++ Veri_Odakli_Gelistirme/Store/StoreApp/obj/Debug/net8.0/Veri_Odakli_Gelistirme.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Veri_Odakli_Gelistirme")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8da502315241ebe94fad37e9b636a24fe3b1721")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e50b84b152a762e4f6cab08d71ac37dc0b401312")]
 [assembly: System.Reflection.AssemblyProductAttribute("Veri_Odakli_Gelistirme")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Veri_Odakli_Gelistirme")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
