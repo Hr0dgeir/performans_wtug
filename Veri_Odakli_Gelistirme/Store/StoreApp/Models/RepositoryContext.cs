cd4481bca83e4a2b54218df407497500dbe2829d
Kaan Kaya <151462786+Hr0dgeir@users.noreply.github.com>
2025-02-27 20:18:33 +0300

    4. Veri Odaklı Geliştirme Bölümü ders 4.6 migrations ne olduğu öğrenildi veri tabanı dosyaları oluşturuldu çekirdek veriler oluşturulud

--- Veri_Odakli_Gelistirme/Store/StoreApp/Models/RepositoryContext.cs
+++ Veri_Odakli_Gelistirme/Store/StoreApp/Models/RepositoryContext.cs
@@ -6,9 +6,22 @@ namespace StoreApp.Models
     {
         public DbSet<Product> Products { get; set; }
         public RepositoryContext(DbContextOptions<RepositoryContext> options)
-        :base(options)
+        : base(options)
         {
-             
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>()
+            .HasData(
+                new Product() { ProductId = 1, ProductName = "Computer", Price = 17_000 },
+                new Product() { ProductId = 2, ProductName = "Keyboard", Price = 1000 },
+                new Product() { ProductId = 3, ProductName = "Mouse", Price = 500 },
+                new Product() { ProductId = 4, ProductName = "Monitor", Price = 7000 },
+                new Product() { ProductId = 5, ProductName = "Deck", Price = 1500 }
+            );
         }
     }
 }
