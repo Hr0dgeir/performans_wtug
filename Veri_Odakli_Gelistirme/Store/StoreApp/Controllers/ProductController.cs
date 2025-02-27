d399993c2235b8f9308ca04e2b6e7ba13b1ee1bc
Kaan Kaya <151462786+Hr0dgeir@users.noreply.github.com>
2025-02-27 21:02:54 +0300

    4. Veri Odaklı Geliştirme Bölümü ders 4.7 veri tabanından verilerin çekilip işlenmesi üzerine çalışıldı yeni dizinler ve dosyalar oluşturuldu

---
+++ Veri_Odakli_Gelistirme/Store/StoreApp/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+
+namespace Name.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly RepositoryContext _context;
+
+        public ProductController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(){
+
+            /*
+            var context = new RepositoryContext(
+                new DbContextOptionsBuilder<RepositoryContext>()
+                .UseSqlite("Data Source = C:\\MVC\\ProductDb.db")
+                .Options);
+            */
+
+            var model = _context.Products.ToList();
+            return View(model);
+        }
+    }
+}
